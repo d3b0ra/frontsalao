fc1cbf0c6d5e137b435d5fd9c96dd6e928110ce9
d3b0ra <deborac02@hotmail.com>
2022-06-25 10:50:52 -0400

    resource salao implementado

--- home.php
+++ home.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
 
-    <body>
+    <body> 
         <?php
         include "topo.php";       
         ?>       
